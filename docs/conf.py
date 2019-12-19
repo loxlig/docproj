e0ffa0247cfd82c26237faabe52168568f56a674
Len Bright <lpb@lowell.edu>
2019-12-19 13:45:10 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,6 @@ import sphinx_rtd_theme
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.append('/home/lpb/docproj/docs/docproj')
 
 # -- Project information -----------------------------------------------------
 
